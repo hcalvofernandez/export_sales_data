52a46390814a08131697b4ced23269da2552a04d
Yoinier Hernandez Nieves <ynievesdotnet@gmail.com>
2020-08-09 12:27:29 -0400

    only save the column names

--- wizards/export_sales_data.py
+++ wizards/export_sales_data.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from odoo import api, fields, models, _
+import io, csv, base64
 from odoo.exceptions import MissingError
 
 class ExportSalesDat(models.TransientModel):
@@ -8,4 +9,12 @@ class ExportSalesDat(models.TransientModel):
     _description = 'Export Sales Data'
 
     def export_sales_data_action(self):
-        raise MissingError(_('OK'))
+        # order_recs = order_obj.search([('date_order','<=',previous_start_date),('date_order','>=',previous_new_rec_date)]).ids
+        pos_orders = self.env['pos.order'].search_read([],[])
+        lines = []
+        for rec in pos_orders:  
+            lines.append(rec)        
+        with open('export_sales_data.csv', 'w') as fp:
+            a = csv.writer(fp, delimiter=',')            
+            data_lines = lines
+            a.writerows(data_lines)
