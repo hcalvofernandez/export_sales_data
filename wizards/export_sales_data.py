d1955521e65f394b4df53e00cc31bfcf727465c8
Yoinier Hernandez Nieves <ynievesdotnet@gmail.com>
2020-08-09 16:07:24 -0400

    Added Date filter

--- wizards/export_sales_data.py
+++ wizards/export_sales_data.py
@@ -4,6 +4,7 @@ from odoo import api, fields, models, _
 import io
 import csv
 import base64
+import os
 from odoo.exceptions import MissingError
 
 
@@ -11,9 +12,11 @@ class ExportSalesDat(models.TransientModel):
     _name = 'export.sales.data'
     _description = 'Export Sales Data'
 
+    start_date = fields.Date(string='Start Date')
+    end_date = fields.Date(string='End Date')
+
     def export_sales_data_action(self):
-        # pos_orders = self.env['pos.order'].search([('date_order','<=',previous_start_date),('date_order','>=',previous_new_rec_date)]).ids
-        pos_orders = self.env['pos.order'].search_read([], [])
+        pos_orders = self.env['pos.order'].search_read([('date_order','>=',self.start_date),('date_order','<=',self.end_date)], [])
         download_file = "export_sales_data.csv"
         columnTitleRow = [pos_orders[0]]
         with open(download_file, 'w') as fp:
