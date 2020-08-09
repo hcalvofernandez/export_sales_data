df11404f3daeea5167624f67152caed50a302e7c
Yoinier Hernandez Nieves <ynievesdotnet@gmail.com>
2020-08-09 14:15:19 -0400

    Write the correct file

--- wizards/export_sales_data.py
+++ wizards/export_sales_data.py
@@ -1,8 +1,11 @@
 # -*- coding: utf-8 -*-
 
 from odoo import api, fields, models, _
-import io, csv, base64
+import io
+import csv
+import base64
 from odoo.exceptions import MissingError
+
 
 class ExportSalesDat(models.TransientModel):
     _name = 'export.sales.data'
@@ -10,11 +13,14 @@ class ExportSalesDat(models.TransientModel):
 
     def export_sales_data_action(self):
         # pos_orders = self.env['pos.order'].search([('date_order','<=',previous_start_date),('date_order','>=',previous_new_rec_date)]).ids
-        pos_orders = self.env['pos.order'].search_read([],[])
-        lines = []
-        for rec in pos_orders:  
-            lines.append(rec)        
-        with open('export_sales_data.csv', 'w') as fp:
-            a = csv.writer(fp, delimiter=',')            
-            data_lines = lines
-            a.writerows(data_lines)
+        pos_orders = self.env['pos.order'].search_read([], [])
+        download_file = "export_sales_data.csv"
+        columnTitleRow = [pos_orders[0]]
+        with open(download_file, 'w') as fp:
+            rowWrite = csv.writer(fp, delimiter=',')
+            rowWrite.writerows(columnTitleRow)
+            for dic in pos_orders:
+                for key in dic.keys():
+                    row = str(dic[key])+","
+                    fp.write(row)
+                fp.write('\n')
